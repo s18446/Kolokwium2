eeae988e6e5d53ecbe00d43026f17aaab1262caf
lekhi <danek@poczta.onet.eu>
2020-06-18 11:50:21 +0200

    koniec

---
+++ Kolokwium2/Controllers/PlayersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium2.Controllers
+{
+    [Route("api/teams/{id:int}/players")]
+    public class PlayersController : ControllerBase
+    {
+
+        [HttpPut]
+        IActionResult AddPlayer(int id)
+        {
+
+            return Ok();
+        }
+
+
+
+
+    }
+}
